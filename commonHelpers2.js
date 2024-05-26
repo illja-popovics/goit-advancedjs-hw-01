615e96885a34979568e5a285a0bb80892f2339fd
illja-popovics <143592959+illja-popovics@users.noreply.github.com>
2024-05-26 15:22:09 +0000

    Deploying to gh-pages from @ illja-popovics/goit-advancedjs-hw-01@1a3909ea7557576b37cc17c77d219f0c76908ecb 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */import{P as a,t as m}from"./assets/vendor-1e54b975.js";const c=document.querySelector("#vimeo-player"),o=new a(c),r="videoplayer-current-time",t=localStorage.getItem(r);t&&o.setCurrentTime(t).catch(e=>{console.error("Failed to set current time:",e)});const i=m(({seconds:e})=>{localStorage.setItem(r,e)},1e3);o.on("timeupdate",i);
+//# sourceMappingURL=commonHelpers2.js.map
